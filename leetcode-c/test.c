8ac0ccf6af32414dea9fe774f7d5073781faa40d
foxleezh <foxleezh@gmail.com>
2019-01-29 14:27:53 +0800

    解决第八题

--- leetcode-c/test.c
+++ leetcode-c/test.c
@@ -1,10 +1,10 @@
 #include "include/env.h"
 
-extern void solution7(void);
+extern void solution8(void);
 
 int main() {
 	//_CrtSetBreakAlloc(81);
-	solution7();
+	solution8();
 	_CrtDumpMemoryLeaks();
 	int i = 0;
 	scanf("%d",&i);
