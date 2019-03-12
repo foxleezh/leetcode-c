1c5111843f684d3068de748c104dd3ced867c03a
foxleezh <foxleezh@gmail.com>
2019-03-12 14:25:41 +0800

    解决第25题

--- leetcode-c/test.c
+++ leetcode-c/test.c
@@ -4,11 +4,11 @@
  * 要测试请把solution后面的数字改为对应的题目
  */
 
-extern void solution21(void);
+extern void solution25(void);
 
 int main() {
 	//_CrtSetBreakAlloc(76);
-	solution21();
+	solution25();
 	_CrtDumpMemoryLeaks();
 	int i = 0;
 	scanf("%d",&i);
